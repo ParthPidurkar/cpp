b6b4a4159e6e2bd8e23bbd1844e2edc93f244809
ParthPidurkar <85994476+ParthPidurkar@users.noreply.github.com>
2023-08-24 21:34:13 +0530

    Add files via upload

---
+++ Week 1 Basics/arithmetic_operations.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 5 , y = 5, z;
+    cout<<"x = "<<x<<endl<<"y = "<<y;
+    cout<<"preincrement : "<<++x;
+    
+
+     
+    return 0;
+}
---
+++ Week 1 Basics/division.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout<<"\nEnter the value of X :";
+    cin>>x;
+    cout<<"\nEnter the value of Y :";
+    cin>>y;
+    cout<<"\n Qu : "<<x/y<<endl; 
+    
+    return 0;
+}
